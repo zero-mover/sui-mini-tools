402fdd99a00556d1c572ea4c157bbf92c986b904
lizhengda <zero17280124@gmail.com>
2024-05-13 12:42:03 +0800

    save

--- src/account.rs
+++ src/account.rs
@@ -2,19 +2,18 @@
 
 use std::fs::{self,File};
 use std::io::Write;
-use std::path::{Path,PathBuf};
+use std::path::{self,Path,PathBuf};
+use fastcrypto::ed25519::Ed25519KeyPair;
 use serde::{Serialize, Deserialize};
+use sui_sdk::rpc_types::Coin;
 use sui_types::{ 
     base_types::SuiAddress, 
     crypto::{get_key_pair, EncodeDecodeBase64, SuiKeyPair},
 };
 use shared_crypto::intent::Intent;
-use fastcrypto::hash::{HashFunction,HashFunctionWrapper};
+use anyhow::{anyhow, Ok};
 
-
-use anyhow::anyhow;
-
-use sui_keys::keystore::{AccountKeystore, FileBasedKeystore, Keystore};
+use sui_keys::keystore::{self, AccountKeystore, FileBasedKeystore};
 use sui_keys::keypair_file::write_keypair_to_file;
 
 use sui_sdk::{
@@ -24,70 +23,77 @@ use sui_sdk::{
         transaction::{Argument, Command, Transaction, TransactionData},
         quorum_driver_types::ExecuteTransactionRequestType,
     },
-    SuiClientBuilder,
     SuiClient
 };
-
-use futures::{future, stream::StreamExt};
 
 #[derive(Serialize, Deserialize,Debug)]
 pub struct AddressKeyPair {
     pub account: String,
     pub keypair: String
 }
-#[tokio::main]
 pub  async fn build_tranfer_tx(
-    client: SuiClient,
-    path: &PathBuf,
+    client: &SuiClient,
+    path: &str,
     recipient: SuiAddress,
     amount: u64,
+    coin: Coin, //total(gas+transfer amount)
     gas_buget: u64,
-    coin_type: String
-)-> Result<SuiTransactionBlockResponse, anyhow::Error> {
+   // coin_type: String
+)-> Result<(), anyhow::Error> {
     //check path valid
-    let keystore = FileBasedKeystore::new(path).unwrap();
+    println!("path is {:?} ", Path::new(path).exists());
+
+    if  !Path::new(path).exists() {
+        return Err(anyhow!("key_store file no exist"))
+    }
+    
+    let key_store_file=PathBuf::from(path);
+
+    //  key_store_file
+
+    let keystore = FileBasedKeystore::new(&key_store_file)?;
     // default use first account 
     let sender=keystore.addresses()[0];
-    
-    //let signature = keystore.sign_secure(&sender, &tx_data, Intent::sui_transaction())?;
-   // let sender_addr: SuiAddress=(&sender.public()).into();
-    let coins_stream=client.coin_read_api().get_coins_stream(sender,Some(coin_type));
-    let mut coins = coins_stream
-        .skip_while(|c| future::ready(c.balance < 5_0_000_000))
-        .boxed();
-    let coin =  coins.next().await.unwrap();
-    let mut ptb = ProgrammableTransactionBuilder::new();
 
-    // 2) split coin
-    // the amount we want in the new coin, 1000 MIST
-    let split_coint_amount = ptb.pure(amount).unwrap(); // note that we need to specify the u64 type
-    ptb.command(Command::SplitCoins(
-        Argument::GasCoin,
-        vec![split_coint_amount],
-    ));
 
-    // 3) transfer the new coin to a different address
-    let argument_address = ptb.pure(recipient).unwrap();
-    ptb.command(Command::TransferObjects(
-        vec![Argument::Result(0)],
-        argument_address,
-    ));
-    let gas_price = client.read_api().get_reference_gas_price().await.unwrap();
-    // finish building the transaction block by calling finish on the ptb
-    let builder = ptb.finish();
+    println!(" keystore sender is {:?} ", sender);
 
-    let tx_data: TransactionData=TransactionData::new_programmable(sender, vec![coin.object_ref()], builder, gas_buget, gas_price);
-    let signature = keystore.sign_secure(&sender, &tx_data, Intent::sui_transaction())?;
 
-    let transaction_response: sui_sdk::rpc_types::SuiTransactionBlockResponse = client
-    .quorum_driver_api()
-    .execute_transaction_block(
-        Transaction::from_data(tx_data, vec![signature]),
-        SuiTransactionBlockResponseOptions::full_content(),
-        Some(ExecuteTransactionRequestType::WaitForLocalExecution),
-    )
-    .await?;
-    Ok(transaction_response)
+
+    // let mut ptb = ProgrammableTransactionBuilder::new();
+
+    // // 2) split coin
+    // // the amount we want in the new coin, 1000 MIST
+    // let split_coint_amount = ptb.pure(amount).unwrap(); // note that we need to specify the u64 type
+    // ptb.command(Command::SplitCoins(
+    //     Argument::GasCoin,
+    //     vec![split_coint_amount],
+    // ));
+
+    // // 3) transfer the new coin to a different address
+    // let argument_address = ptb.pure(recipient).unwrap();
+    // ptb.command(Command::TransferObjects(
+    //     vec![Argument::Result(0)],
+    //     argument_address,
+    // ));
+    // let gas_price = client.read_api().get_reference_gas_price().await.unwrap();
+    // // finish building the transaction block by calling finish on the ptb
+    // let builder = ptb.finish();
+
+    // let tx_data: TransactionData=TransactionData::new_programmable(sender, vec![coin.object_ref()], builder, gas_buget, gas_price);
+    // let signature = keystore.sign_secure(&sender, &tx_data, Intent::sui_transaction())?;
+
+
+    // let transaction_response: sui_sdk::rpc_types::SuiTransactionBlockResponse = client
+    // .quorum_driver_api()
+    // .execute_transaction_block(
+    //     Transaction::from_data(tx_data, vec![signature]),
+    //     SuiTransactionBlockResponseOptions::full_content(),
+    //     Some(ExecuteTransactionRequestType::WaitForLocalExecution),
+    // )
+    // .await?;
+    // Ok(transaction_response)
+    Ok(())
 
 }
 
@@ -98,6 +104,7 @@ pub fn batch_generate_account(num_keypairs: usize,path: &str)-> Result<(), anyho
         File::open(path).unwrap()
     };
     let mut keypair: Vec<AddressKeyPair> = Vec::new();
+
     for _ in 0..num_keypairs {
         let kp_ed = SuiKeyPair::Ed25519(get_key_pair().1);
         let addr_ed: SuiAddress = (&kp_ed.public()).into();
@@ -112,15 +119,41 @@ pub fn batch_generate_account(num_keypairs: usize,path: &str)-> Result<(), anyho
     Ok(())
 }
 
-pub fn batch_generate_account_to_file(num_keypairs: usize,path: &str,key_store_path: &str)-> Result<(), anyhow::Error>{
-    fs::create_dir_all(key_store_path).unwrap();
+
+pub fn batch_create_account_to_key_store_file(num_keypairs: usize,json_path: &str,key_store_file: &str)-> Result<(), anyhow::Error>{
+    //
+    let mut file= if !Path::new(json_path).exists(){
+        File::create(json_path).unwrap()
+    }else{
+        File::open(json_path).unwrap()
+    };
+    let mut keystores=FileBasedKeystore::new(&PathBuf::from(key_store_file))?;
+    let mut jons_pair: Vec<AddressKeyPair> = Vec::new();
+
+    for _ in 0..num_keypairs {
+        let kp_ed = SuiKeyPair::Ed25519(get_key_pair().1);
+        let addr_ed: SuiAddress = (&kp_ed.public()).into();
+        let address_keypair = AddressKeyPair {
+            account: addr_ed.to_string(),
+            keypair: kp_ed.encode_base64(),
+        };
+        jons_pair.push(address_keypair);
+        keystores.add_key(None, kp_ed).unwrap();
+    };
+    let json_str = serde_json::to_string_pretty(&jons_pair)?;
+    file.write_all(json_str.as_bytes())?;
+    Ok(())
+}
+
+
+pub fn batch_generate_account_to_file(num_keypairs: usize,path: &str,key_store_dir: &str)-> Result<(), anyhow::Error>{
+    fs::create_dir_all(key_store_dir).unwrap();
     let mut file=if !Path::new(path).exists(){
         File::create(path).unwrap()
     }else{
         File::open(path).unwrap()
     };
-
-    let dir_path = Path::new(key_store_path);
+    let dir_path = Path::new(key_store_dir);
     let mut keypair: Vec<AddressKeyPair> = Vec::new();
 
 
@@ -136,8 +169,6 @@ pub fn batch_generate_account_to_file(num_keypairs: usize,path: &str,key_store_p
         write_keypair_to_file(&kp_ed,&key_path).unwrap();
         keypair.push(address_keypair)
     };
-
-
     let json_str = serde_json::to_string_pretty(&keypair)?;
     file.write_all(json_str.as_bytes())?;
     Ok(())
@@ -146,13 +177,6 @@ pub fn batch_generate_account_to_file(num_keypairs: usize,path: &str,key_store_p
 
 
 pub fn read_account_info(path: &str)-> Result<Vec<AddressKeyPair>, anyhow::Error>{
-    // let mut keypair: Vec<AddressKeyPair> = Vec::new();
-    // if !Path::new(path).exists(){
-    //     return Err("file no exist".into());
-    // }else{
-    //     File::open(path);
-    // };
-
     let file=fs::read_to_string(path).unwrap();
     // let mut file: Result<File, std::io::Error> =File::open(path);
     let keypair: Vec<AddressKeyPair>=serde_json::from_str(&file).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,202 +1,57 @@
+use serde_json::from_str;
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-
-use stable_test::{batch_generate_account,read_account_info,read_keypair};
-
-use fastcrypto::encoding::Encoding;
-use fastcrypto::hash::HashFunction;
-use fastcrypto::{
-    ed25519::Ed25519KeyPair,
-    encoding::Base64,
-    secp256k1::Secp256k1KeyPair,
-    secp256r1::Secp256r1KeyPair,
-    traits::{EncodeDecodeBase64, KeyPair},
-};
-// use rand::{rngs::StdRng, SeedableRng};
-use shared_crypto::intent::{Intent, IntentMessage};
+use stable_test::{batch_generate_account, batch_generate_account_to_file, build_tranfer_tx, read_account_info, read_keypair,batch_generate_account_to_key_store_file};
+use sui_sdk::{SuiClientBuilder,types::base_types::{ObjectID, SuiAddress}};
+use std::{path::PathBuf, str::FromStr};
 use futures::{future, stream::StreamExt};
-use sui_sdk::{
-    rpc_types::SuiTransactionBlockResponseOptions,
-    types::{
-        programmable_transaction_builder::ProgrammableTransactionBuilder,
-        transaction::{Argument, Command, Transaction, TransactionData},
-        digests::TransactionDigest,
-    },
-    SuiClientBuilder,
-};
-use sui_types::crypto::Signer;
-use sui_types::crypto::SuiSignature;
-use sui_types::crypto::ToFromBytes;
-use sui_types::signature::GenericSignature;
-use sui_types::{
-    base_types::SuiAddress,
-    crypto::{get_key_pair_from_rng, SuiKeyPair},
-};
+use tokio::task;
 
+use sui_keys::keystore::{self, AccountKeystore, FileBasedKeystore};
 
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    //batch_generate_account(100, "./address1.json");
-    let sui_client = SuiClientBuilder::default().build_testnet().await?;
+    //batch_generate_account_to_key_store_file(100,"./address_data.json","./key_store.keystore")?;
+    let accounts=read_account_info("./address_data.json")?;
 
-    let coin_type = "0x2::sui::SUI".to_string();
-    let data=read_account_info("./address1.json")?;
+
+// //    let _ =batch_generate_account_to_file(100,"./address_info.json","./key_store/");
     let client = SuiClientBuilder::default().build_testnet().await?;
-    let key: SuiKeyPair=read_keypair(&data[0].keypair).unwrap();
-    let sender: SuiAddress=(&key.public()).into();
-    let second=read_keypair(&data[0].keypair).unwrap();
-    let receiver: SuiAddress=(&second.public()).into();
-    let coins_stream=client.coin_read_api().get_coins_stream(sender,Some(coin_type));
-    let mut coins = coins_stream
-        .skip_while(|c| future::ready(c.balance < 5_0_000_000))
-        .boxed();
 
-    let coin = coins.next().await.unwrap();
+    let keystore = FileBasedKeystore::new(&PathBuf::from("./key_store.keystore"))?;
+    // default use first account 
+    let sender=keystore.addresses();
+    let ss:SuiAddress=accounts[0].account.parse()?;
+    let xd=keystore.get_key(&ss).unwrap().copy();
+    let addr_ed:SuiAddress=(&xd.public()).into();
 
-    let mut ptb = ProgrammableTransactionBuilder::new();
+    // let addr_ed: SuiAddress =xd.into();
 
-    let split_coint_amount = ptb.pure(1000u64)?; // note that we need to specify the u64 type
-
-    ptb.command(Command::SplitCoins(
-        Argument::GasCoin,
-        vec![split_coint_amount],
-    ));
-
-    let argument_address = ptb.pure(receiver)?;
-    ptb.command(Command::TransferObjects(
-        vec![Argument::Result(0)],
-        argument_address,
-    ));
-    let gas_price = client.read_api().get_reference_gas_price().await?;
-
-    let builder = ptb.finish();
+    println!("{:?} {:?}",addr_ed,ss);
 
 
-    let tx_data=TransactionData::new_programmable(sender, vec![coin.object_ref()], builder, 300000, gas_price);
+   //;
+    //println!("{:?}",keystore.get_key(&ss).unwrap());
 
-    let intent_msg = IntentMessage::new(Intent::sui_transaction(), tx_data);
-    // let dat=tx_data.into();
-    //let raw_tx = bcs::to_bytes(&intent_msg).expect("bcs should not fail");
-    //let mut hasher=sui_types::crypto::DefaultHash::default();
+
+//     // let sender_addr: SuiAddress =from_str(r#""0x7db19e51e43548723854bc1f0dca86f2138fda6ad9a4f404e48c86d4f68b5f29""#)?;
+
+//     let arr=read_account_info("/Users/lizhengda/myspace/learning_rust/stable_test/address_info.json").unwrap();
+//     let sender: SuiAddress=(&arr[0].account).parse()?;
+//     let receiver: SuiAddress=(&arr[1].account).parse()?;
+// //    let signature = keystore.sign_secure(&sender, &tx_data, Intent::sui_transaction())?;
+// //    let sender_addr: SuiAddress=(&sender.public()).into();
+//     let coins_stream=client.coin_read_api().get_coins_stream(sender,Some("0x2::sui::SUI".to_string()));
+//     let mut coins = coins_stream
+//         .skip_while(|c| future::ready(c.balance < 5_0_000_000))
+//         .boxed();
+//     let coin =  coins.next().await.unwrap();
+
+//     let resopnse=build_tranfer_tx(&client, "/Users/lizhengda/myspace/learning_rust/stable_test/key_store/0.keystore", receiver, 5_0_000_000, coin, 30000000).await.unwrap();
+//     //std::thread::sleep(std::time::Duration::from_secs(1));
+//     println!("{:?}",resopnse);
+
     
-    //hasher.update(raw_tx.clone());
-    // let digest = hasher.finalize().digest;
-
-
-    // let sui_sig=key.sign(&digest);
-
-    // let res = sui_sig.verify_secure(
-    //     &intent_msg,
-    //     sender,
-    //     sui_types::crypto::SignatureScheme::ED25519,
-    // );
-    // assert!(res.is_ok());
-
-
-    //  // execute the transaction.
-    //  let transaction_response = client
-    //  .quorum_driver_api()
-    //  .execute_transaction_block(
-    //      sui_types::transaction::Transaction::from_generic_sig_data(
-    //          intent_msg.value,
-    //          vec![GenericSignature::Signature(sui_sig)],
-    //      ),
-    //      SuiTransactionBlockResponseOptions::default(),
-    //      None,
-    //  )
-    //  .await?;
-
-    // let tx_data: TransactionData=TransactionData::new_programmable(kind, sender, gas_payment, gas_budget, gas_price)
-
-    // let x=Keystore::from(key);
-    
-    // FileBasedKeystore::new(path)
-    
-    
-    //key.borrow()
-    // Keystore::from(value)
-    
-    // println!("{:?}");
-
-    // let mut file = File::create("account.json").unwrap();
-    // //let mut keystore = Keystore::from(FileBasedKeystore::new(&keystore_path).unwrap());
-
-
-    // let (address,key)=get_account_key_pair();
-
-    // file.write_all(buf)
-
-    //println!("{:?}",json_str);
-
-    // // 1) get the Sui client, the sender and recipient that we will use
-    // // for the transaction, and find the coin we use as gas
-    // let (sui, sender, recipient) = setup_for_write().await?;
-
-    // // we need to find the coin we will use as gas
-    // let coins = sui
-    //     .coin_read_api()
-    //     .get_coins(sender, None, None, None)
-    //     .await?;
-    // let coin = coins.data.into_iter().next().unwrap();
-
-    // // programmable transactions allows the user to bundle a number of actions into one transaction
-    // let mut ptb = ProgrammableTransactionBuilder::new();
-
-    // // 2) split coin
-    // // the amount we want in the new coin, 1000 MIST
-    // let split_coint_amount = ptb.pure(1000u64)?; // note that we need to specify the u64 type
-    // ptb.command(Command::SplitCoins(
-    //     Argument::GasCoin,
-    //     vec![split_coint_amount],
-    // ));
-
-    // // 3) transfer the new coin to a different address
-    // let argument_address = ptb.pure(recipient)?;
-    // ptb.command(Command::TransferObjects(
-    //     vec![Argument::Result(0)],
-    //     argument_address,
-    // ));
-
-    // // finish building the transaction block by calling finish on the ptb
-    // let builder = ptb.finish();
-
-    // let gas_budget = 5_000_000;
-    // let gas_price = sui.read_api().get_reference_gas_price().await?;
-    // // create the transaction data that will be sent to the network
-    // let tx_data = TransactionData::new_programmable(
-    //     sender,
-    //     vec![coin.object_ref()],
-    //     builder,
-    //     gas_budget,
-    //     gas_price,
-    // );
-
-    // // 4) sign transaction
-    // let keystore = FileBasedKeystore::new(&sui_config_dir()?.join(SUI_KEYSTORE_FILENAME))?;
-    // let signature = keystore.sign_secure(&sender, &tx_data, Intent::sui_transaction())?;
-
-    // // 5) execute the transaction
-    // print!("Executing the transaction...");
-    // let transaction_response = sui
-    //     .quorum_driver_api()
-    //     .execute_transaction_block(
-    //         Transaction::from_data(tx_data, vec![signature]),
-    //         SuiTransactionBlockResponseOptions::full_content(),
-    //         Some(ExecuteTransactionRequestType::WaitForLocalExecution),
-    //     )
-    //     .await?;
-    // print!("done\n Transaction information: ");
-    // println!("{:?}", transaction_response);
-
-    // let coins = sui
-    //     .coin_read_api()
-    //     .get_coins(recipient, None, None, None)
-    //     .await?;
-
-    // println!(
-    //     "After the transfer, the recipient address {recipient} has {} coins",
-    //     coins.data.len()
-    //);
     Ok(())
 }
